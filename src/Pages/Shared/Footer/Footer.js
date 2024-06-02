4733795ae1d37562cbf50d35b8e21ded9adc1880
Kutub Udd <socialnetwork98710@gmail.com>
2024-06-02 13:48:55 +0600

    added

--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -6,8 +6,8 @@ const Footer = () => {
   const date = new Date();
   const currentYear = date.getFullYear();
   return (
-    <div>
-      <footer className="px-4 divide-y bg-gray-100 text-gray-800 px-[10%]">
+    <div className="FooterBox">
+      <footer className="  px-4 divide-y  text-gray-800 px-[10%]">
         <div className="container flex flex-col justify-between py-10 mx-auto space-y-8 lg:flex-row lg:space-y-0">
           <div className="lg:w-1/3">
             <a
